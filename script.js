942525309983522beb25f25fffd808b9eabfc268
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-01 09:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+
+window.onresize =  () =>{
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+	console.log("l")
+}
+;
